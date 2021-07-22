bb1edc41251b6fd3f29b8438d6e19f5a80952065
abzel <32515091+abzel@users.noreply.github.com>
2021-07-22 16:28:58 +0600

    restart function

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function Input({ label, children, className, ...attrs }) {
+  // console.log(label);
+  // console.log(children);
+  // console.log(attrs);
+  // console.log(className);
   return (
     <div>
       {label && (
--- src/store/reducers/reposReducer.js
+++ src/store/reducers/reposReducer.js
@@ -1,28 +1,40 @@
 import {
-  DRAW_O,
-  RESTART_BOARD,
-  DRAW_X,
-  PLAYER_O,
-  PLAYER_X,
+    DRAW_O,
+    RESTART_BOARD,
+    DRAW_X,
+    PLAYER_O,
+    PLAYER_X,
 } from "../action/action";
 
 const initialState = {
-  size: 3,
-  moves: 0,
-  boardSize: Math.pow(3, 2),
-  winner: "",
-  player: null,
+    size: 3,
+    moves: 0,
+    boardSize: Math.pow(3, 2),
+    winner: "",
+    player: '',
 };
 
 export const reposReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case DRAW_X:
-      return {};
-    case DRAW_X:
-      return {};
-    case RESTART_BOARD:
-      return {};
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case DRAW_X:
+            const newXState = [...state];
+            newXState[action.cellIndex] = "X";
+            return newXState;
+
+        case DRAW_O:
+            const newOState = [...state];
+            newOState[action.cellIndex] = "O";
+            return newOState;
+        case RESTART_BOARD:
+            return {
+                ...state,
+                size: action.obj.size,
+                moves: state.moves,
+                winner: action.obj.winner,
+                player: action.obj.player,
+            };
+
+        default:
+            return state;
+    }
 };
